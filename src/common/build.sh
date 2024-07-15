e1400b49ce0ed54d6f48cf630ae3f99bd55b1436
simhue <huening.simon@gmail.com>
2024-07-15 13:58:08 +0200

    update .deb-package from 1.5.3 to 1.6.0

--- src/common/build.sh
+++ src/common/build.sh
@@ -18,11 +18,17 @@ fi
 # Superdirectory this script is located in + /resources, namely src/resources
 readonly DIR_RESOURCES="$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")" &>/dev/null && pwd)/resources"
 
+readonly DIR_DOWNLOADS="./downloads"
+
 function init_build_environment() {
     set -a
     . "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)/versions"
     set +a
     mkdir -p "${DIR_BUILD}"
+
+    if [ ! -d "${DIR_DOWNLOADS}" ]; then
+      mkdir "${DIR_DOWNLOADS}"
+    fi
 }
 
 function clean_up_build_environment() {
@@ -32,9 +38,13 @@ function clean_up_build_environment() {
 function download_i2b2_webclient() {
     local DIR_WEBCLIENT="${1}"
 
-    wget "https://github.com/i2b2/i2b2-webclient/archive/v${VERSION_I2B2_WEBCLIENT}.zip" -P "${DIR_BUILD}"
-    unzip "${DIR_BUILD}/v${VERSION_I2B2_WEBCLIENT}.zip" -d "${DIR_BUILD}"
-    rm "${DIR_BUILD}/v${VERSION_I2B2_WEBCLIENT}.zip"
+    if [ ! -f "${DIR_DOWNLOADS}/v${VERSION_I2B2_WEBCLIENT}.zip" ]; then
+      echo "Download i2b2 webclient v${VERSION_I2B2_WEBCLIENT}"
+      wget "https://github.com/i2b2/i2b2-webclient/archive/v${VERSION_I2B2_WEBCLIENT}.zip" -P "${DIR_DOWNLOADS}"
+    fi
+
+    unzip "${DIR_DOWNLOADS}/v${VERSION_I2B2_WEBCLIENT}.zip" -d "${DIR_BUILD}"
+
     mkdir -p "$(dirname "${DIR_BUILD}${DIR_WEBCLIENT}")"
     mv "${DIR_BUILD}/i2b2-webclient-${VERSION_I2B2_WEBCLIENT}" "${DIR_BUILD}${DIR_WEBCLIENT}"
 }
@@ -42,8 +52,8 @@ function download_i2b2_webclient() {
 function config_i2b2_webclient() {
     local DIR_WEBCLIENT="${1}"
 
-    sed -i "s|name: \"HarvardDemo\",|name: \"AKTIN\",|" "${DIR_BUILD}${DIR_WEBCLIENT}/i2b2_config_data.js"
-    sed -i "s|urlCellPM: \"http://services.i2b2.org/i2b2/services/PMService/\",|urlCellPM: \"http://127.0.0.1:9090/i2b2/services/PMService/\",|" "${DIR_BUILD}${DIR_WEBCLIENT}/i2b2_config_data.js"
+    cp ${DIR_RESOURCES}/i2b2_config/i2b2_config_domains.json ${DIR_BUILD}${DIR_WEBCLIENT}/i2b2_config_domains.json
+
     sed -i "s|loginDefaultUsername : \"demo\"|loginDefaultUsername : \"\"|" "${DIR_BUILD}${DIR_WEBCLIENT}/js-i2b2/i2b2_ui_config.js"
     sed -i "s|loginDefaultPassword : \"demouser\"|loginDefaultPassword : \"\"|" "${DIR_BUILD}${DIR_WEBCLIENT}/js-i2b2/i2b2_ui_config.js"
 }
@@ -51,9 +61,13 @@ function config_i2b2_webclient() {
 function download_wildfly() {
     local DIR_WILDFLY_HOME="${1}"
 
-    wget "https://download.jboss.org/wildfly/${VERSION_WILDFLY}/wildfly-${VERSION_WILDFLY}.zip" -P "${DIR_BUILD}"
-    unzip "${DIR_BUILD}/wildfly-${VERSION_WILDFLY}.zip" -d "${DIR_BUILD}"
-    rm "${DIR_BUILD}/wildfly-${VERSION_WILDFLY}.zip"
+    if [ ! -f "${DIR_DOWNLOADS}/wildfly-${VERSION_WILDFLY}.zip" ]; then
+      echo "Download Wildfly ${VERSION_WILDFLY}"
+#      wget "https://download.jboss.org/wildfly/${VERSION_WILDFLY}/wildfly-${VERSION_WILDFLY}.zip"  -P "${DIR_DOWNLOADS}"
+      wget "https://github.com/wildfly/wildfly/releases/download/${VERSION_WILDFLY}/wildfly-${VERSION_WILDFLY}.zip" -P "${DIR_DOWNLOADS}"
+    fi
+
+    unzip "${DIR_DOWNLOADS}/wildfly-${VERSION_WILDFLY}.zip" -d "${DIR_BUILD}"
     mkdir -p "$(dirname "${DIR_BUILD}${DIR_WILDFLY_HOME}")"
     mv "${DIR_BUILD}/wildfly-${VERSION_WILDFLY}" "${DIR_BUILD}${DIR_WILDFLY_HOME}"
 }
@@ -88,14 +102,25 @@ function config_wildfly() {
 function download_wildfly_jdbc() {
     local DIR_WILDFLY_DEPLOYMENTS="${1}"
 
-    wget "https://jdbc.postgresql.org/download/postgresql-${VERSION_POSTGRES_JDBC}.jar" -P "${DIR_BUILD}${DIR_WILDFLY_DEPLOYMENTS}"
+    if [ ! -f "${DIR_DOWNLOADS}/postgresql-${VERSION_POSTGRES_JDBC}.jar" ]; then
+      echo "Download Postgres ${VERSION_POSTGRES_JDBC}"
+      wget "https://jdbc.postgresql.org/download/postgresql-${VERSION_POSTGRES_JDBC}.jar" -P "${DIR_DOWNLOADS}"
+    fi
+
+    cp "${DIR_DOWNLOADS}/postgresql-${VERSION_POSTGRES_JDBC}.jar" "${DIR_BUILD}${DIR_WILDFLY_DEPLOYMENTS}"
 }
 
 function download_wildfly_i2b2() {
     local DIR_WILDFLY_DEPLOYMENTS="${1}"
 
-    # TODO load i2b2 from official sources
-    wget "https://www.aktin.org/software/repo/org/i2b2/${VERSION_I2B2}/i2b2.war" -P "${DIR_BUILD}${DIR_WILDFLY_DEPLOYMENTS}"
+    if [ ! -f "${DIR_DOWNLOADS}/i2b2core-upgrade-${VERSION_I2B2}.zip" ]; then
+      echo "i2b2core-upgrade-${VERSION_I2B2}.zip not found. Please download i2b2core-upgrade-${VERSION_I2B2}.zip from https://www.i2b2.org/software/index.html and move it to ${DIR_DOWNLOADS}."
+      exit 1
+    fi
+
+    unzip -j "${DIR_DOWNLOADS}/i2b2core-upgrade-${VERSION_I2B2}.zip" "i2b2/deployments/i2b2.war" \
+          -d "${DIR_BUILD}${DIR_WILDFLY_DEPLOYMENTS}"
+
 }
 
 function copy_database_for_postinstall() {
